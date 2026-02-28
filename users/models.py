8ecb844c46e1843879e84afef599ce32aa9d68b3
hendrix-llouchi <henricobb2@gmail.com>
2026-02-28 01:06:57 +0000

    feat: add post_save signal to auto-create UserProfile on User creation

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    firebase_uid = models.CharField(max_length=128, unique=True)
-    username = models.CharField(max_length=150, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
+    firebase_uid = models.CharField(max_length=128, unique=True, blank=True, null=True)
     bio = models.TextField(blank=True)
     profile_photo_url = models.URLField(blank=True)
     interests = models.JSONField(default=list)
@@ -11,4 +12,4 @@ class UserProfile(models.Model):
     xp = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.username
+        return self.user.username
