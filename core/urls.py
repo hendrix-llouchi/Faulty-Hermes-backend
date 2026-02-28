9263ebd27b64785253ccc942c9a3098424c8a872
hendrix-llouchi <henricobb2@gmail.com>
2026-02-28 01:02:05 +0000

    feat: add JWT auth and user registration endpoint

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('lessons.urls')),
+    path('api/v1/users/', include('users.urls')),
+    path('api/v1/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
